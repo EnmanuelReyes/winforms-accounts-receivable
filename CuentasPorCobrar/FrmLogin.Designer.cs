4a3c16add44d472fb1f411e42ddcf56e08d8ee1f
Enmanuel Reyes <enmanuelreyes@live.com>
2016-10-09 10:19:59 -0400

    Implementing Login Feature

--- CuentasPorCobrar/FrmLogin.Designer.cs
+++ CuentasPorCobrar/FrmLogin.Designer.cs
@@ -28,33 +28,102 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.CmbLogin = new System.Windows.Forms.Button();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cuentaPorCobtrarDBDataSet = new CuentasPorCobrar.CuentaPorCobtrarDBDataSet();
+            this.cuentaPorCobtrarDBDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.cuentaPorCobtrarDBDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cuentaPorCobtrarDBDataSetBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // CmbLogin
             // 
-            this.CmbLogin.Location = new System.Drawing.Point(67, 141);
+            this.CmbLogin.Location = new System.Drawing.Point(97, 108);
             this.CmbLogin.Name = "CmbLogin";
-            this.CmbLogin.Size = new System.Drawing.Size(75, 23);
+            this.CmbLogin.Size = new System.Drawing.Size(123, 32);
             this.CmbLogin.TabIndex = 0;
             this.CmbLogin.Text = "Entrar";
             this.CmbLogin.UseVisualStyleBackColor = true;
             this.CmbLogin.Click += new System.EventHandler(this.CmbLogin_Click);
             // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(68, 32);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(187, 20);
+            this.txtUser.TabIndex = 1;
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(68, 73);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.PasswordChar = '*';
+            this.txtPass.Size = new System.Drawing.Size(187, 20);
+            this.txtPass.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Usuario";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Clave";
+            // 
+            // cuentaPorCobtrarDBDataSet
+            // 
+            this.cuentaPorCobtrarDBDataSet.DataSetName = "CuentaPorCobtrarDBDataSet";
+            this.cuentaPorCobtrarDBDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // cuentaPorCobtrarDBDataSetBindingSource
+            // 
+            this.cuentaPorCobtrarDBDataSetBindingSource.DataSource = this.cuentaPorCobtrarDBDataSet;
+            this.cuentaPorCobtrarDBDataSetBindingSource.Position = 0;
+            // 
             // FrmLogin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(313, 265);
+            this.ClientSize = new System.Drawing.Size(280, 161);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtUser);
             this.Controls.Add(this.CmbLogin);
             this.Name = "FrmLogin";
-            this.Text = "FrmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.FrmLogin_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.cuentaPorCobtrarDBDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cuentaPorCobtrarDBDataSetBindingSource)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button CmbLogin;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private CuentaPorCobtrarDBDataSet cuentaPorCobtrarDBDataSet;
+        private System.Windows.Forms.BindingSource cuentaPorCobtrarDBDataSetBindingSource;
     }
 }
--- CuentasPorCobrar/FrmLogin.cs
+++ CuentasPorCobrar/FrmLogin.cs
@@ -1,30 +1,67 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace CuentasPorCobrar
 {
     public partial class FrmLogin : Form
     {
+        private string _client;
+        private string _idIdentification;
         public FrmLogin()
         {
             InitializeComponent();
+            _client = "test";
+            _idIdentification = "1234";
         }
 
         private void CmbLogin_Click(object sender, EventArgs e)
         {
             //TODO: validate login
-
+            FrmLogin frmLogin = new FrmLogin();
             FrmMenu frmMenu = new FrmMenu();
             Hide();
 
-            frmMenu.Show();
+            try
+            {
+                if ((string.IsNullOrWhiteSpace(txtUser.Text) &&
+                    string.IsNullOrWhiteSpace(txtPass.Text)) ||
+                    _client != txtUser.Text && _idIdentification != txtPass.Text)
+                {
+                    MessageBox.Show("Credenciales incorrectas", " Error",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                    frmLogin.Show();
+                }
+                else
+                {
+                    if (txtUser.Text != null && txtPass.Text != null)
+                    {
+                        if (_client != null && _idIdentification != null)
+                        {
+                            if (_client == txtUser.Text && _idIdentification == txtPass.Text)
+                            {
+                                frmMenu.Show();
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                throw;
+            }
+        }
+
+        public void Validateuser(string client, string idIdentification)
+        {
+
+            //Todo: Crear mejor implementacion para validar credenciales.
+        }
+
+        private void FrmLogin_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
